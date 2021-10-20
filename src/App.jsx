b4ffa520cda6b5b5257de58dee4c5def96143181
Claiton <cdpaulus@inf.ufsm.br>
2021-10-19 21:31:46 -0300

    Experience stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,123 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+import ReactToPrint from "react-to-print";
+import { Container, Item, ItemData, ItemCommands, ItemRow, Input, TextArea, Button, Title, ContainerScroll } from './Styles';
 
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
+  const componentRef = useRef(null)
+  const [collapse, setCollapse] = useState(false);
+  const [content, setContent] = useState("<div>teste</div>");
+
+  const [experiences, setExperiences] = useState([{
+    company: "",
+    job: "",
+    dti: "",
+    dtf: "",
+    tasks: "",
+  }])
+
+  const ComponentToPrint = () => {
+    return (
+      <div dangerouslySetInnerHTML={{__html: content}}></div>
+    );
+  }
+
+  const addItem = () => {
+    let aux = experiences;
+    aux.push({
+      company: "",
+      job: "",
+      dti: "",
+      dtf: "",
+      tasks: "",
+    });
+    setExperiences([...aux]);
+  }
+
+  const removeItem = (index) => {
+    let aux = experiences;
+    aux.splice(index, 1);
+    setExperiences([...aux]);
+  }
+
+  const updateField = (index, field, e) => {
+    let aux = experiences;
+    aux[index][field] = e;
+    setExperiences([...aux]);
+  }
+
+  const ContentEditor = () => {
+    return(
+      <div style={{padding: "20px"}}>
+        <Container collapse={collapse}>
+          <Title>
+            <span>Experience</span>
+            <span onClick={() => setCollapse(!collapse)}>{collapse ? "Y": "X"}</span>
+          </Title>
+          <ContainerScroll>
+            {experiences.map((exp, index) => (
+              <Item>
+                <ItemData>
+                  <ItemRow>
+                    <Input 
+                      value={exp.company} 
+                      onChange={(e) => updateField(index, "company", e.target.value)} 
+                      placeholder="Company" 
+                    />
+                    <Input 
+                      value={exp.job} 
+                      onChange={(e) => updateField(index, "job", e.target.value)} 
+                      placeholder="Job" 
+                    />
+                  </ItemRow>
+                  <ItemRow>
+                    <Input 
+                      value={exp.dti} 
+                      onChange={(e) => updateField(index, "dti", e.target.value)} 
+                      type="date"
+                    />
+                    <Input 
+                      value={exp.dtf} 
+                      onChange={(e) => updateField(index, "dtf", e.target.value)} 
+                      type="date"
+                    />
+                  </ItemRow>
+                  <ItemRow>
+                    <TextArea 
+                      value={exp.tasks}
+                      placeholder="Tasks performed"
+                      onChange={(e) => updateField(index, "tasks", e.target.value)} 
+                    />
+                  </ItemRow>
+                </ItemData>
+                <ItemCommands>
+                  {index !== 0 &&
+                    <Button plus={false} onClick={() => removeItem(index)}>-</Button>
+                  }
+                  <Button plus={true} onClick={() => addItem()}>+</Button>
+                </ItemCommands>
+              </Item>
+            ))}
+          </ContainerScroll>
+        </Container>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <div className="Content">
+        {ContentEditor()}
+      </div>
+      <div className="Preview">
+        <div ref={componentRef}>
+          <ComponentToPrint />
+        </div>
+         <ReactToPrint
+          trigger={() => <button>Print</button>}
+          content={() => componentRef.current}
+         />
+      </div>
     </div>
   );
 }
---
+++ src/Styles.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  border: 1px solid #c4c4c4;
+  border-radius: 8px;
+  height: ${({ collapse }) => collapse && "60px"};
+  overflow: ${({ collapse }) => (collapse ? "hidden" : "auto")}; ;
+`;
+
+export const ContainerScroll = styled.div`
+  max-height: 400px;
+  overflow: ${({ collapse }) => (collapse ? "hidden" : "auto")}; ;
+`;
+
+export const Item = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 30px;
+`;
+
+export const ItemData = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 85%;
+`;
+
+export const ItemCommands = styled.div`
+  display: flex;
+  width: 15%;
+`;
+
+export const ItemRow = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const Input = styled.input`
+  width: 42%;
+  border-radius: 8px;
+  padding: 8px 16px;
+  display: block;
+  background-color: rgb(242, 245, 250);
+  border: 0px;
+  pointer-events: unset;
+  margin-bottom: 10px;
+`;
+
+export const TextArea = styled.textarea`
+  width: 92% !important;
+  border-radius: 8px;
+  padding: 8px 16px;
+  width: 100%;
+  background-color: rgb(242, 245, 250);
+  border: 0px;
+  pointer-events: unset;
+  margin-bottom: 10px;
+`;
+
+export const Button = styled.button`
+  background: ${({ plus }) => (plus ? "#b1ec7a" : "#ffbd3f")};
+  margin-right: ${({ plus }) => !plus && "5px"};
+  border: none;
+  width: 40px;
+  height: 40px;
+  border-radius: 100%;
+  cursor: pointer;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 10px;
+`;
